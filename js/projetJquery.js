48ccabdc257fc91b4cb779c08a221e31bc067c17
Oumou <coumou100@gmail.com>
2023-05-06 13:25:20 +0000

    deploiment comeUp

---
+++ js/projetJquery.js
@@ -0,0 +1,182 @@
+
+$('body').css("margin", "0");
+$("li").css("list-style-type", "none");
+$("a").css("text-decoration", "none")
+    .css("color", "black")
+
+// debut section header
+
+$('header').css("background-color", "#FFEE66")
+    .css("font-size", '15px')
+    .css("padding-left", "100px")
+    .css("padding-right", "100px");
+
+$('header nav').css('display', "flex")
+    .css("width", "auto")
+    .css("align-items", 'center')
+    .css("justify-content", "space-around");
+
+$('header nav ul li span').css('color', "white")
+    .css('background-color', 'black')
+    .css('padding', '10px');
+
+$('header .search input').css('padding', "10px")
+    .css("font-size", '12px')
+    .css('width', '300px');
+
+$('header .search button').css('padding', "10px")
+    .css('margin-left', '-5px')
+    .css('background-color', "black");
+
+$('header i').css('color', 'white')
+
+
+$('header ul').css('display', "flex")
+    .css('text-transform', 'uppercase');
+
+$('header li').css('padding', '10px')
+    .css('list-style-type', 'none');
+
+$('header .accueil').css('display', "flex")
+    .css('justify-content', 'space-around');
+
+$('.text-accueil p').css('font-size', '60px');
+
+$('.text-accueil input').css('padding', "20px")
+    .css("font-size", '15px')
+    .css('width', '400px');
+
+$('header .text-accueil button').css('padding', "20px")
+    .css('margin-left', '-5px')
+    .css('background-color', "black");
+
+// fin section header
+
+// debut section categories
+$('.logo-mini-1').css('margin-top', "-55px")
+    .css("margin-left", "120px");
+$(".logo-mini-1 img").css("width", "120px");
+
+$(".categories").css("justify-content", "center")
+    .css("padding", "15px")
+    .css("display", "flex")
+    .css('flex-wrap', "wrap");
+
+$(".categories h2").css("width", "auto")
+    .css('font-size', '40px');
+
+$('.categories ul').css('flex-wrap', "wrap")
+    .css("display", "flex")
+    .css("justify-content", "space-around");
+
+$(".categories li").css("font-size", "25px")
+    .css("margin-right", "30px")
+    .css("margin-left", "30px")
+    .css("width", '200px')
+    .css('text-align', 'center');
+
+// fin section categories
+
+// début section prestataire
+
+$('.prestataire').css("background-color", "black")
+    .css("display", "flex")
+    .css('margin-bottom', '200px');
+
+$(".prestataire img").css("width", "360")
+    .css("margin", "70px")
+    .css('margin-left', '200px');
+
+$('.prestataire h1').css("color", "#FFEE66")
+    .css("font-size", "50px");
+
+$('.prestataire button').css("padding", "10px")
+    .css("padding-right", "35px")
+    .css('font-size', "20px")
+    .css('font-weight', 'bold')
+    .css("padding-left", "35px")
+    .css("background-color", "white")
+    .css('border', 'none');
+
+$('.prestataire-texte').css('text-align', "center")
+    .css('position', "absolute")
+    .css('width', "600px")
+    .css("margin-left", "350px")
+    .css("margin-top", "90px");
+
+$(".prestataire .symbole").css('align-self', 'flex-end')
+    .css("margin-bottom", "150px")
+    .css("margin-left", "100px");
+
+$('.prestataire .o-list').css("background-color", "#FFEE66")
+    .css('align-self', 'flex-end')
+    .css('font-size', '32px')
+    .css('font-weight', 'bold')
+    .css("text-align", "right")
+    .css('margin-bottom', "-100px")
+    .css('margin-left', "-500px")
+    .css('padding', '25px')
+
+$('.trois').css('text-decoration', "underline");
+$('.prestataire .o-list li').css('list-style-type', 'decimal');
+
+// fin section prestataire
+
+// début section vendeur
+
+$('.vendeur').css('background-color', "#FFBBAA")
+    .css('margin', '120px 200px 120px 200px')
+    .css('display', 'flex');
+
+$('.vendeur img').css('width', '360px')
+    .css('margin', "70px 0px 0px 500px");
+
+
+$('.vendeur h1').css('font-size', '50px')
+    .css("color", "white")
+    .css('line-height', '1.1')
+    .css('width', '600px');
+
+$('.vendeur button').css("padding", "10px")
+    .css("padding-right", "45px")
+    .css('font-size', "20px")
+    .css('font-weight', 'bold')
+    .css("padding-left", "45px")
+    .css("background-color", "black")
+    .css('color', 'white')
+    .css('border', 'none');
+
+$('.vendeur .vendeur-texte').css('position', 'absolute')
+    .css("margin", "10px 0px 0px 80px")
+
+// fin section vendeur
+
+//début footer
+
+$("footer").css('background-color', '#F3F3F3')
+    .css('padding', '20px 200px 100px');
+
+$('footer .partie-superieur').css('display', 'flex')
+    .css('justify-content', 'space-between');
+
+$('footer .paiement').css('display', 'flex')
+    .css('justify-content', 'space-between');
+
+$('footer .paiement ul').css('display', 'flex')
+    .css('justify-content', 'space-between');
+
+$('footer .statistiques span').css('border', 'solid 1px')
+    .css('padding', '7px')
+    .css('font-weight', 'bold')
+    .css("font-size", '15px');
+
+$('footer h3').css('font-size', '25px')
+$('footer .statistiques').css('line-height', '1.7')
+    .css('margin-top', '-25px');
+
+$('footer .apropos').css('line-height', '1.7')
+    .css('margin-top', '-25px');
+
+$('footer .apropos a').css('border-bottom', 'solid 1px gray')
+
+//fin footer
